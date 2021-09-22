831ddd7dbfc8ae899449552f12790d547b839e69
kevin <kevchen1129@gmail.com>
2021-09-22 13:12:14 -0500

    test git

--- app/src/main/java/com/example/basiccalculatorapp/MainActivity.java
+++ app/src/main/java/com/example/basiccalculatorapp/MainActivity.java
@@ -6,8 +6,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
